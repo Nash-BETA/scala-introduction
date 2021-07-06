4a17c49b33743c3ecde9f51b1ec2b99643509997
Nash-BEAT <nash.0cal@gmail.com>
2021-07-07 02:49:09 +0900

    save

---
+++ introduction-session2/shape.scala
@@ -0,0 +1,19 @@
+abstract class Shape {
+
+  def draw(): Unit = {
+    println("不明な図形")
+  }
+}
+
+class Triangle extends Shape {
+  override def draw(): Unit = {
+    println("三角形")
+  }
+}
+
+class Rectangle extends Shape {
+  override def draw(): Unit = {
+    println("四角形")
+  }
+}
+class UnknownShape extends Shape
